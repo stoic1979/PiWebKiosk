e8796eb255978eb6be7ef4277e3405ce7842559f
Navjot Singh <weavebytes@gmail.com>
2017-08-04 15:17:05 +0530

    updated setup file

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 # setup script for dependencies
 #
 sudo apt-get install libwebkit-dev
-sudo apt install python-webki
+sudo apt install python-webkit
 sudo pip install PyGTK
+sudo pip install python-gtk2
